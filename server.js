cfe147c71d78c85a87bbc8ab26c929c2aa8ca539
Ram Mishra <er.ram.ku.mishra@gmail.com>
2024-05-28 12:15:26 +0530

    updated

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.set("view engine", "ejs");
 
 // load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
-app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
 
 // load routers
